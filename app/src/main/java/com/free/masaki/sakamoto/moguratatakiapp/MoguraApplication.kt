4fbcbc01f607c4820d5b0524c52bdb5510d05637
坂本将紀 <sbl.sakamoto.masaki@gmail.com>
2026-05-12 00:28:46 +0900

    Applicationクラスの追加。

---
+++ app/src/main/java/com/free/masaki/sakamoto/moguratatakiapp/MoguraApplication.kt
@@ -0,0 +1,7 @@
+package com.free.masaki.sakamoto.moguratatakiapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MoguraApplication : Application()
